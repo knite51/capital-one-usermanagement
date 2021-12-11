621ccb0cb8157d26ef83e8072a26c024915606a9
knitedeveloper <knite51@gmail.com>
2021-12-11 16:19:32 +0100

    add route features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+
+  {
+    path: 'users',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+  },
+
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: '**',
+    redirectTo: 'auth/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
